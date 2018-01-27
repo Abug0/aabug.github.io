5387c82c9be71e7ab6e21a17472cfbab9b94e016
Abug0 <1341785032@qq.com>
2018-01-27 12:03:12 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Welcome to myWorld</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=gbk">
+		<link rel="stylesheet" href="home.css">
+		<script src="js/jQuery.js"></script>
+	</head>
+	
+	<body>
+    Hello World!
+  </body>
+  </html>
